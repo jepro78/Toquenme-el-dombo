237a719f5f044f2708a3b7b0a267b416a8aa6e9e
stevenrm-dev <stevenroldanm@gmail.com>
2021-02-18 12:42:01 +0100

    Dombo medio hacer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
-const audio = document.getElementById("sound1");
-const play0 = document.getElementById("dombo1");
+let onsound1 = document.getElementById("sound1");
+let onsound2 = document.getElementById("sound2");
+let onsound3 = document.getElementById("sound3");
+let onsound4 = document.getElementById("sound4");
 
-/*play0.addEventListener("click", () => {
-    audio.play();
+let dombobtn1 = document.querySelector(".dombo1");
+let dombobtn2 = document.querySelector(".dombo2");
+let dombobtn3 = document.querySelector(".dombo3");
+let dombobtn4 = document.querySelector(".dombo4");
+
+dombobtn1.addEventListener("click", () => {
+    onsound1.load();
+    onsound1.play();
+});
+
+dombobtn2.addEventListener("click", () => {
+    onsound2.load();
+    onsound2.play();
+});
+
+dombobtn3.addEventListener("click", () => {
+    onsound3.load();
+    onsound3.play();
+});
+
+dombobtn4.addEventListener("click", () => {
+    onsound4.load();
+    onsound4.play();
 });
